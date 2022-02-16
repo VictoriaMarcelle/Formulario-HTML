895cec3205d7bb13996825b9d957f8f5b69ff417
Victoria Maciel <victoria.marcellemm@gmail.com>
2022-02-16 10:11:11 -0300

    Envio da 3 questão da atividade 1 de javascript

---
+++ objetos_aula_17_02.js
@@ -0,0 +1,7 @@
+let carro = {marca:'fiat', ano: '2003', cor:'roxo'} //criação de objetos
+
+//let carro = novo objeto () instanciação
+console.log('marca atual do carro: ${carro.marca}')
+
+carro.marca = 'Mercedes'
+carro.cor = 'vermelho'
