d112fb440ebf22b4ff0b315ceef9c06d85f73f9c
Victoria Maciel <victoria.marcellemm@gmail.com>
2022-02-16 10:15:25 -0300

    Remoção de arquivos desnecessários

--- objetos_aula_17_02.js
+++
@@ -1,7 +0,0 @@
-let carro = {marca:'fiat', ano: '2003', cor:'roxo'} //criação de objetos
-
-//let carro = novo objeto () instanciação
-console.log('marca atual do carro: ${carro.marca}')
-
-carro.marca = 'Mercedes'
-carro.cor = 'vermelho'
