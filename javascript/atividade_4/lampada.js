8a04d786610b54677740ae812a4a4b04a5913ff8
Victoria Maciel <victoria.marcellemm@gmail.com>
2022-02-25 16:05:52 -0300

    Atividade 4 não finalizada

---
+++ javascript/atividade_4/lampada.js
@@ -0,0 +1,10 @@
+function amit()
+{
+var a= document.getElementById("img");
+var b= document.getElementById("btn")
+if(screen.value=="Acesa"){
+    a.src="img/lampada-apagada.png";
+}
+else {
+    a.src="img/lampada-acesa.png";
+}}
