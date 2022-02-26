f47a364be4eb31f2b7aa374b5f0dbc4563852dd0
Victoria Maciel <victoria.marcellemm@gmail.com>
2022-02-26 20:01:18 -0300

    Atividade 4 finalizada

--- javascript/atividade_4/lampada.js
+++ javascript/atividade_4/lampada.js
@@ -1,10 +1,18 @@
-function amit()
-{
-var a= document.getElementById("img");
-var b= document.getElementById("btn")
-if(screen.value=="Acesa"){
-    a.src="img/lampada-apagada.png";
+function acendeApagaLampada(){
+    var imagem = document.getElementById("img");
+    var botao = document.getElementById("btn"); 
+
+    if (botao.value === 'apagada') {
+        botao.value = 'acesa';
+        botao.innerHTML = 'Apagar';
+        botao.setAttribute('class', 'lampada-acesa');
+
+        imagem.setAttribute('src', 'img/lampada-acesa.png');
+    } else {
+        botao.value = 'apagada';
+        botao.innerHTML = 'Acender';
+        botao.setAttribute('class', 'lampada-apagada');
+
+        imagem.setAttribute('src', 'img/lampada-apagada.png');
+    }
 }
-else {
-    a.src="img/lampada-acesa.png";
-}}
